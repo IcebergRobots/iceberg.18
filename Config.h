9ba8d1934b976b46bbe12931e4688b9d137fa0d1
Unknown <honigbrot@wolke7.net>
2017-12-06 16:02:07 +0100

    drive 0.2

---
+++ Config.h
@@ -0,0 +1,4 @@
+#define DEBUG true
+
+#define debug(_str_); if(DEBUG){Serial.print(_str_);}
+#define debugln(_str_); if(DEBUG){Serial.println(_str_);}
--- pin.h
+++ pin.h
@@ -1,20 +1,20 @@
 #ifndef pin_h
 #define pin_h
 
-#define FWD1  0
-#define BWD1  0
-#define PWM1  0
+#define FWD1  34
+#define BWD1  36
+#define PWM1  13
 
-#define FWD2  0
-#define BWD2  0
-#define PWM2  0
+#define FWD2  30
+#define BWD2  32
+#define PWM2  10
 
-#define FWD3  0
-#define BWD3  0
-#define PWM3  0
+#define FWD3  44
+#define BWD3  42
+#define PWM3  11
 
-#define FWD4  0
-#define BWD4  0
-#define PWM4  0
+#define FWD4  38
+#define BWD4  40
+#define PWM4  12
 
 #endif
