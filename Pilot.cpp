71d48d11f11f20a5ef42cf645ff568966231de25
Unknown <honigbrot@wolke7.net>
2017-12-11 14:15:35 +0100

    umkommentierung
    bessere kommentare für Pilot Klasse

--- Pilot.cpp
+++ Pilot.cpp
@@ -4,22 +4,21 @@
 const int sinus[360] = {0,175,349,523,698,872,1045,1219,1392,1564,1736,1908,2079,2250,2419,2588,2756,2924,3090,3256,3420,3584,3746,3907,4067,4226,4384,4540,4695,4848,5000,5150,5299,5446,5592,5736,5878,6018,6157,6293,6428,6561,6691,6820,6947,7071,7193,7314,7431,7547,7660,7771,7880,7986,8090,8192,8290,8387,8480,8572,8660,8746,8829,8910,8988,9063,9135,9205,9272,9336,9397,9455,9511,9563,9613,9659,9703,9744,9781,9816,9848,9877,9903,9925,9945,9962,9976,9986,9994,9998,10000,9998,9994,9986,9976,9962,9945,9925,9903,9877,9848,9816,9781,9744,9703,9659,9613,9563,9511,9455,9397,9336,9272,9205,9135,9063,8988,8910,8829,8746,8660,8572,8480,8387,8290,8192,8090,7986,7880,7771,7660,7547,7431,7314,7193,7071,6947,6820,6691,6561,6428,6293,6157,6018,5878,5736,5592,5446,5299,5150,5000,4848,4695,4540,4384,4226,4067,3907,3746,3584,3420,3256,3090,2924,2756,2588,2419,2250,2079,1908,1736,1564,1392,1219,1045,872,698,523,349,175,0,-175,-349,-523,-698,-872,-1045,-1219,-1392,-1564,-1736,-1908,-2079,-2250,-2419,-2588,-2756,-2924,-3090,-3256,-3420,-3584,-3746,-3907,-4067,-4226,-4384,-4540,-4695,-4848,-5000,-5150,-5299,-5446,-5592,-5736,-5878,-6018,-6157,-6293,-6428,-6561,-6691,-6820,-6947,-7071,-7193,-7314,-7431,-7547,-7660,-7771,-7880,-7986,-8090,-8192,-8290,-8387,-8480,-8572,-8660,-8746,-8829,-8910,-8988,-9063,-9135,-9205,-9272,-9336,-9397,-9455,-9511,-9563,-9613,-9659,-9703,-9744,-9781,-9816,-9848,-9877,-9903,-9925,-9945,-9962,-9976,-9986,-9994,-9998,-10000,-9998,-9994,-9986,-9976,-9962,-9945,-9925,-9903,-9877,-9848,-9816,-9781,-9744,-9703,-9659,-9613,-9563,-9511,-9455,-9397,-9336,-9272,-9205,-9135,-9063,-8988,-8910,-8829,-8746,-8660,-8572,-8480,-8387,-8290,-8192,-8090,-7986,-7880,-7771,-7660,-7547,-7431,-7314,-7193,-7071,-6947,-6820,-6691,-6561,-6428,-6293,-6157,-6018,-5878,-5736,-5592,-5446,-5299,-5150,-5000,-4848,-4695,-4540,-4384,-4226,-4067,-3907,-3746,-3584,-3420,-3256,-3090,-2924,-2756,-2588,-2419,-2250,-2079,-1908,-1736,-1564,-1392,-1219,-1045,-872,-698,-523,-349,-175};
 
 /*****************************************************
-Konstruktor
+setze Achsenwinkel auf 70°
 *****************************************************/
 Pilot::Pilot(){
   _angle = 70;
 }
 
 /*****************************************************
-Konstruktor(angle)
-angle = Winkel zwischen zwei Motoren einer Seite
+setze Achsenwinkel
 *****************************************************/
 Pilot::Pilot(byte angle){
   _angle = angle;
 }
 
 /*****************************************************
-gebe Motor-Ansteuerungspins an
+setze Motor-Ansteuerungspins
 *****************************************************/
 void Pilot::setPins(byte id, byte fwd, byte bwd, byte pwm) {
   if(id < 0 || id > 3) {  // ungueltige Eingabe
@@ -36,7 +35,7 @@ void Pilot::setPins(byte id, byte fwd, byte bwd, byte pwm) {
 }
 
 /*****************************************************
-setze den Winkel zwischen zwei Motoren einer Seite
+setze den Winkel zwischen zwei Motoren einer Seite (Achsenwinkel)
 *****************************************************/
 void Pilot::setAngle(byte angle) {
   _angle = angle%180;
@@ -62,7 +61,7 @@ void Pilot::steerMotor(byte id, int power) {
 }
 
 /*****************************************************
-steuere die Motoren an
+steuere die Motoren an, um zu fahren
 - keine Berechnungen
 *****************************************************/
 void Pilot::drive() {
@@ -70,7 +69,7 @@ void Pilot::drive() {
 }
 
 /*****************************************************
-steuere die Motoren an
+steuere die Motoren an, um zu fahren
 - nutze Berechnungen des Zwischenspeichers
 *****************************************************/
 void Pilot::drive(int values[]) {
@@ -80,8 +79,7 @@ void Pilot::drive(int values[]) {
 }
 
 /*****************************************************
-steuere die Motoren an
-- berechne Zwischenspeicher mit Winkel und Geschwindigkeit
+fahre mit Geschwindigkeit und Zielwinkel
 *****************************************************/
 void Pilot::drive(int angle, int power) {
   calculate(angle, power);
@@ -89,8 +87,15 @@ void Pilot::drive(int angle, int power) {
 }
 
 /*****************************************************
-steuere die Motoren an
-- berechne Zwischenspeicher mit Winkel, Geschwindigkeit, Rotation
+fahre mit Geschwindigkeit, Zielwinkel und Eigenrotation
+- Geschwindigkeit [0 bis 255]
+- Zielwinkel [-180 bis 180]:
+      0  
+    .--.
+90 /    \ -90
+   \    /
+- Eigenrotation [-255 bis 255]
+-> Korrekturdrehung, um wieder zum Gegnertor ausgerichtet zu sein
 *****************************************************/
  void Pilot::drive(int angle, int power, int rotation) {
   calculate(angle, power, rotation);
@@ -98,22 +103,14 @@ steuere die Motoren an
 }
 
 /*****************************************************
-berechne den Zwischenspeicher
-- aus Winkel und Stärke
+berechne Zwischenspeicher für Motoransteuerung
 *****************************************************/
 void Pilot::calculate(int angle, int power) {
   calculate(angle, power, 0);
 }
 
 /*****************************************************
-berechne den Zwischenspeicher
-angle = Zielwinkel
--> gegen den Uhrzeigersinn
--> vorne: 0°
--> -180 bis 180
-power = Geschwindigkeit
--> 0 bis 255
-rotation = Korrekturdrehung, um wieder zum Gegnertor ausgerichtet zu sein
+berechne Zwischenspeicher für Motoransteuerung
 *****************************************************/
 void Pilot::calculate(int angle, int power, int rotation) {
   if(power<0) {
