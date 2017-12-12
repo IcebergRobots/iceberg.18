3b951b595f9a8c34af8482e820abb22ecb564e40
Unknown <finn_harms@web.de>
2017-12-12 22:34:26 +0100

    Kompentierung main
    Ein paar Kommentare in der Iceberg.ino

--- iceberg.ino
+++ iceberg.ino
@@ -7,34 +7,36 @@
 #include "HMC6352.h"
 
 #define PWR 50
+  
+Pilot m;            //Motorobjekt
+HMC6352 c;  //Kompassobjekt
 
-Pilot m;
-HMC6352 myCompass;
-
-int heading;
-int startHeading;
-int rotation;
+int heading;        //Wert des Kompass
+int startHeading;   //Startwert des Kompass
+int rotation;       //rotationswert für die Motoren
 
 void setup() {
+  //Start-fiep
   for(int i = 10; i<2000; i += 10){
     tone(BUZZER, i);
     delay(1);
   }
   
-  Serial.begin(9600);
-  Wire.begin();
-  pinModes();
-  motorConfig();
-  myCompass.setOutputMode(0);
+  Serial.begin(9600);   //Start der Seriellen Kommunikation
+  Wire.begin();         //Start der I2C-Kommunikation
+  
+  pinModes();           //setzt die PinModes
+  motorConfig();        //konfiguriert das Pilot-Objekt
+  c.setOutputMode(0);   //keine Ahnung
 
-  startHeading = myCompass.getHeading();
+  startHeading = c.getHeading();  //merkt sich den Startwert des Kompass
 }
 
 void loop(){
-  heading = ((int)((myCompass.getHeading()-startHeading)+360) % 360)-180;
-  rotation = map(heading,-180,180,-PWR,PWR);
+  heading = ((int)((c.getHeading()-startHeading)+360) % 360)-180;   //Misst die Kompassabweichung vom Startwert
+  rotation = map(heading,-180,180,-PWR,PWR);                        //Berechnet die Rotation zu Kompasskorrektur
   
-  m.drive(0, PWR-abs(rotation),rotation);
+  m.drive(0, PWR-abs(rotation),rotation);                           //steuert die Motoren an
 }
 
 void motorConfig(){
