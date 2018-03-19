2f32802acaa8ae64da01546dec1ee58814b806cc
Anton Pusch <anton.pusch@outlook.de>
2018-03-19 22:46:21 +0100

    moved setupMotor

--- Utility.cpp
+++ Utility.cpp
@@ -84,3 +84,14 @@ void transmitHeartbeat() {
   mate.send(data, 9); // heartbeat
 }
 
+/*****************************************************
+  Piloten konfigurieren
+*****************************************************/
+void setupMotor() {
+  m.setAngle(70);
+
+  m.setPins(0, FWD0, BWD0, PWM0);
+  m.setPins(1, FWD1, BWD1, PWM1);
+  m.setPins(2, FWD2, BWD2, PWM2);
+  m.setPins(3, FWD3, BWD3, PWM3);
+}
--- iceberg.ino
+++ iceberg.ino
@@ -500,16 +500,6 @@ void loop() {
 
 //###################################################################################################
 
-// Piloten konfigurieren
-void setupMotor() {
-  m.setAngle(70);
-
-  m.setPins(0, FWD0, BWD0, PWM0);
-  m.setPins(1, FWD1, BWD1, PWM1);
-  m.setPins(2, FWD2, BWD2, PWM2);
-  m.setPins(3, FWD3, BWD3, PWM3);
-}
-
 // Bildschirm konfigurieren und Startschriftzug zeigen
 void setupDisplay() {
   d.begin(SSD1306_SWITCHCAPVCC, 0x3C);  //Initialisieren des Displays
